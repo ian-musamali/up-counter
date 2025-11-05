8db99c7cbb307d89840b4b55a71b384468779ec8
Ian Musamali <ian.musamali12@gmail.com>
2025-11-04 23:35:41 -0500

    Create upcounterTB.v

---
+++ upcounterTB.v
@@ -0,0 +1,17 @@
+// Verilog code for up counter with testbench
+// Testbench Verilog code for up counter
+module upcounter_testbench();
+reg clk, reset;
+wire [3:0] counter;
+
+up_counter dut(clk, reset, counter);
+initial begin 
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+reset=1;
+#20;
+reset=0;
+end
+endmodule
